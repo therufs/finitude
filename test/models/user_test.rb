0e9df0eeb6e2d81c44bbc66cc909985b05b48a12
Ruthan <rcfreese@gmail.com>
2014-07-14 22:13:14 -0400

    Add user test and validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    user = User.new(name: "Roberta", email: "roberta@foo.bar")
+  end
+
+  test "User must have name and email" do
+    user1 = User.new(name: "Juan", email: "foo@bar.baz")
+    user2 = User.new(name: "Rosa")
+    user3 = User.new(email: "bar@foo.baz")
+
+    assert user1.save
+    assert !user2.save, "user saved without email!"
+    assert !user3.save, "user saved without name!"
+  end
+
 end
