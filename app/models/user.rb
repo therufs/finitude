8b83ae8b589b4386123cb9bc03bb0d5c6a6bde87
Ruthan <rcfreese@gmail.com>
2014-07-14 22:38:19 -0400

    Add feed tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
+  has_many :feeds
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  setup do
-    user = User.new(name: "Roberta", email: "roberta@foo.bar")
-  end
-
   test "User must have name and email" do
     user1 = User.new(name: "Juan", email: "foo@bar.baz")
     user2 = User.new(name: "Rosa")
@@ -14,6 +10,29 @@ class UserTest < ActiveSupport::TestCase
     assert user1.save
     assert !user2.save, "user saved without email!"
     assert !user3.save, "user saved without name!"
+  end
+
+  test "User can have feeds" do
+    assert users(:user).respond_to?(:feeds)
+  end
+
+  test "User can add feeds" do
+    smittenfeed = Feed.new(url: 'http://feeds.feedburner.com/smittenkitchen',
+                            user_id: user.id)
+    users(:user).feeds << smittenfeed
+    assert user.feeds.contains?(smittenfeed), "#{users[:user].name} can't see smittenfeed"
+  end
+
+  test "User can see own feeds" do
+    flunk "user can't see own feeds"
+  end
+
+  test "User can't see others' feeds" do
+    flunk "user can see others' feeds"
+  end
+
+  test "User must be logged in to see feeds" do
+    flunk "Who can see feeds anyway?"
   end
 
 end
